f9cff6ee590934d67c074b6533e03c3e9a987c2b
Azeem Bande-Ali <me@azeemba.com>
2022-12-03 19:44:06 -0500

    Support creating new personal events  (#17)
    - Pull out navbar and loggedInConditional as components
    - Added a form component to describe the personal event
    - Created a personal events page to display the form
    - Created a personal events API to handle the request

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,8 @@
-import Link from "next/link";
 import Head from "next/head";
-import { Navbar, Button } from "flowbite-react";
 
+import LoggedInConditional from "../components/logged_in_conditional";
 import UserCard from "../components/user_card";
-import Session from "supertokens-auth-react/recipe/session";
-import { redirectToAuth } from "supertokens-auth-react";
-import { signOut } from "supertokens-auth-react/recipe/thirdpartyemailpassword";
-
-async function handleSignout() {
-  await signOut();
-  location.href = "/";
-}
-
-function LoggedInConditional(props: any) {
-  let sessionContext = Session.useSessionContext();
-  if (sessionContext.loading) {
-    return null;
-  }
-  if (sessionContext.doesSessionExist) {
-    return props.ifTrue || null;
-  } else {
-    return props.ifFalse || null;
-  }
-  return null;
-}
+import NavMenu from "../components/navmenu";
 
 export default function Home() {
   return (
@@ -35,37 +14,12 @@ export default function Home() {
       </Head>
 
       <main>
-        <Navbar fluid={true} rounded={true}>
-          <Navbar.Brand href="https://flowbite.com/">
-            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-              Vigorish
-            </span>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse>
-            <Navbar.Link href="/navbars" active={true}>
-              Home
-            </Navbar.Link>
-            <Navbar.Link href="/navbars">About</Navbar.Link>
-            <LoggedInConditional
-              ifTrue={
-                <div>
-                  <Navbar.Link href="/settings">Settings</Navbar.Link>
-                  <Button onClick={() => handleSignout()}>Logout</Button>
-                </div>
-              }
-              ifFalse={<Navbar.Link href="/auth">Login</Navbar.Link>}
-            ></LoggedInConditional>
-          </Navbar.Collapse>
-        </Navbar>
-
+        <NavMenu active="home" />
         <h1 className="text-center">Vigorish</h1>
 
         <p className="text-center">Validate your worldview</p>
 
-        <LoggedInConditional
-          ifTrue={<UserCard></UserCard>}
-        ></LoggedInConditional>
+        <LoggedInConditional ifTrue={<UserCard />} />
       </main>
     </div>
   );
